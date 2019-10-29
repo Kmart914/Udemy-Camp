49ce962c31885d8f4819a0071d277cb60b67a7c4
Kevin Martzolf <kevinmartzolf@Katelyns-MacBook-Pro.local>
2019-10-29 16:19:38 -0700

    more object practice

--- Section-14/Objects/MovieDB/script.js
+++ Section-14/Objects/MovieDB/script.js
@@ -35,3 +35,26 @@ movies.forEach(function(movie){
   console.log(result);
 
 });
+
+var dogSpace = {};
+
+dogSpace.speak = function(){
+  return "WOOF!";
+}
+
+var catSpace = {};
+
+catSpace.speak = function(){
+  return "MEOW"
+};
+
+
+var comments = {};
+
+comments.data = ["111", "123", "93784"];
+
+comments.print = function(){
+  this.data.forEach(function(el){
+    console.log(el);
+  });
+}
